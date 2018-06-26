81d42d2c07575ffa13ec95103f4b3c7dab826877
Psyroos <psyroos@gmail.com>
2018-06-27 01:02:44 +0300

    Added code
    Added code

---
+++ conditions.php
@@ -0,0 +1,53 @@
+<?php
+
+$condions = array(
+	'0' => '<i class="wi wi-tornado"></i>',           // tornado
+	'1' => '<i class="wi wi-rain-wind"></i>',         // tropical storm
+	'2' => '<i class="wi wi-hurricane"></i>',         // hurricane
+	'3' => '<i class="wi wi-thunderstorm"></i>',      // severe thunderstorms
+	'4' => '<i class="wi wi-thunderstorm"></i>',      // thunderstorms
+	'5' => '<i class="wi wi-sleet"></i>',             // mixed rain and snow
+	'6' => '<i class="wi wi-sleet"></i>',             // mixed rain and sleet
+	'7' => '<i class="wi wi-sleet"></i>',             // mixed snow and sleet
+	'8' => '<i class="wi wi-rain-mix"></i>',          // freezing drizzle
+	'9' => '<i class="wi wi-sprinkle"></i>',          // drizzle
+	'10' => '<i class="wi wi-snow"></i>',             // freezing rain
+	'11' => '<i class="wi wi-showers"></i>',          // showers
+	'12' => '<i class="wi wi-showers"></i>',          // showers
+	'13' => '<i class="wi wi-snow-wind"></i>',        // snow flurries
+	'14' => '<i class="wi wi-snow"></i>',             // light snow showers
+	'15' => '<i class="wi wi-cloudy-gusts"></i>',     // blowing snow
+	'16' => '<i class="wi wi-snow"></i>',             // snow
+	'17' => '<i class="wi wi-hail"></i>',             // hail
+	'18' => '<i class="wi wi-sleet"></i>',            // sleet
+	'19' => '<i class="wi wi-dust"></i>',             // dust
+	'20' => '<i class="wi wi-fog"></i>',              // foggy
+	'21' => '<i class="wi wi-day-haze"></i>',         // haze
+	'22' => '<i class="wi wi-smoke"></i>',            // smoky
+	'23' => '<i class="wi wi-strong-wind"></i>',      // blustery
+	'24' => '<i class="wi wi-windy"></i>',            // windy
+	'25' => '<i class="wi wi-snowflake-cold"></i>',   // cold
+	'26' => '<i class="wi wi-cloudy"></i>',           // cloudy
+	'27' => '<i class="wi wi-night-alt-cloudy"></i>', // mostly cloudy (night)
+	'28' => '<i class="wi wi-day-cloudy"></i>',       // mostly cloudy (day)
+	'29' => '<i class="wi wi-night-alt-cloudy"></i>', // partly cloudy (night)
+	'30' => '<i class="wi wi-day-cloudy"></i>',       // partly cloudy (day)
+	'31' => '<i class="wi wi-night-clear"></i>',      // clear (night)
+	'32' => '<i class="wi wi-day-sunny"></i>',        // sunny
+	'33' => '<i class="wi wi-stars"></i>',            // fair (night)
+	'34' => '<i class="wi wi-day-sunny"></i>',        // fair (day)
+	'35' => '<i class="wi wi-hail"></i>',             // mixed rain and hail
+	'36' => '<i class="wi wi-hot"></i>',              // hot
+	'37' => '<i class="wi wi-thunderstorm"></i>',     // isolated thunderstorms
+	'38' => '<i class="wi wi-thunderstorm"></i>',     // scattered thunderstorms
+	'39' => '<i class="wi wi-thunderstorm"></i>',     // scattered thunderstorms
+	'40' => '<i class="wi wi-showers"></i>',          // scattered showers
+	'41' => '<i class="wi wi-snow"></i>',             // heavy snow
+	'42' => '<i class="wi wi-rain-mix"></i>',         // scattered snow showers
+	'43' => '<i class="wi wi-snow"></i>',             // heavy snow
+	'44' => '<i class="wi wi-cloudy"></i>',           // partly cloudy
+	'45' => '<i class="wi wi-storm-showers"></i>',    // thundershowers
+	'46' => '<i class="wi wi-snow"></i>',             // snow showers
+	'47' => '<i class="wi wi-thunderstorm"></i>',     // isolated thundershowers
+	'3200' => '<i class="wi-alien"></i>'              // not available
+);
